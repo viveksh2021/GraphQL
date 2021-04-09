8280b073c7f5a7a9b011341097a02f36a1f8e6d5
vivek.sharma <vivek.sharma@brainvire.com>
2021-04-09 15:47:08 +0530

    graph ql product collection

---
+++ Model/Resolver/ProductsResolver.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+ 
+namespace Brainvire\ProductsGraphQl\Model\Resolver;
+ 
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+ 
+/**
+ * Product collection resolver
+ */
+class ProductsResolver implements ResolverInterface
+{
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+        $this->productRepository = $productRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+ 
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $productsData = $this->getProductsData();
+        return $productsData;
+    }
+ 
+    /**
+     * @return array
+     * @throws GraphQlNoSuchEntityException
+     */
+    private function getProductsData(): array
+    {
+        try {
+            $searchCriteria = $this->searchCriteriaBuilder->addFilter('entity_id', 1,'gteq')->create();
+            $products = $this->productRepository->getList($searchCriteria)->getItems();
+            $productRecord['allProducts'] = [];
+            foreach($products as $product) {
+                $productId = $product->getId();
+                $productRecord['allProducts'][$productId]['sku'] = $product->getSku();
+                $productRecord['allProducts'][$productId]['name'] = $product->getName();
+                $productRecord['allProducts'][$productId]['price'] = $product->getPrice();
+            }
+        } catch (NoSuchEntityException $e) {
+            throw new GraphQlNoSuchEntityException(__($e->getMessage()), $e);
+        }
+        return $productRecord;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Brainvire_ProductsGraphQl',
+    __DIR__
+);
